45088e63da423575d58c481318a128a10d427b97
mustard <mhqnwt@gmail.com>
2022-07-05 18:40:21 +0000

    Add telemetry for connections

--- src/vs/workbench/services/telemetry/browser/telemetryService.ts
+++ src/vs/workbench/services/telemetry/browser/telemetryService.ts
@@ -20,6 +20,8 @@ import { ITelemetryAppender, NullTelemetryService, supportsTelemetry } from 'vs/
 import { IBrowserWorkbenchEnvironmentService } from 'vs/workbench/services/environment/browser/environmentService';
 import { IRemoteAgentService } from 'vs/workbench/services/remote/common/remoteAgentService';
 import { resolveWorkbenchCommonProperties } from 'vs/workbench/services/telemetry/browser/workbenchCommonProperties';
+// eslint-disable-next-line code-import-patterns
+import { GitpodInsightsAppender } from 'vs/gitpod/browser/gitpodInsightsAppender';
 
 export class TelemetryService extends Disposable implements ITelemetryService {
 
@@ -50,6 +52,8 @@ export class TelemetryService extends Disposable implements ITelemetryService {
 				appenders.push(telemetryProvider);
 			}
 			appenders.push(new TelemetryLogAppender(loggerService, environmentService));
+			const gitpodTelemetryProvider: ITelemetryAppender = new GitpodInsightsAppender(productService);
+			appenders.push(gitpodTelemetryProvider);
 			const config: ITelemetryServiceConfig = {
 				appenders,
 				commonProperties: resolveWorkbenchCommonProperties(storageService, productService.commit, productService.version, environmentService.remoteAuthority, productService.embedderIdentifier, productService.removeTelemetryMachineId, environmentService.options && environmentService.options.resolveCommonTelemetryProperties),
