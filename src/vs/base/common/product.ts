1c4a8e68ffdd7a4b82898f5cf196f34bc1032649
Jean Pierre <jeanp413@hotmail.com>
2023-05-23 20:19:32 +0000

    Cleaup analytics

--- src/vs/base/common/product.ts
+++ src/vs/base/common/product.ts
@@ -194,6 +194,7 @@ export interface IProductConfiguration {
 	readonly chatWelcomeView?: IChatWelcomeView;
 	readonly chatParticipantRegistry?: string;
 
+	readonly segmentKey: string;
 	readonly gitpodPreview?: IGitpodPreviewConfiguration;
 }
 
